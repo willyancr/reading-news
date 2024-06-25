2febb71d116f20f145e1052d1f8f99cb1548bee5
Willyan Costa <willyancr@gmail.com>
2024-06-24 21:50:35 -0300

    Update Header Component
    More detailed changes:
    - Refactor the Header component to use a new `SignInButton` component
    - Update the navigation links to use the `transition` class for a smoother hover effect
    - Reorganize the HTML structure for better readability and semantic meaning

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,27 @@
-import { Github } from 'lucide-react';
 import Link from 'next/link';
+import SignInButton from './sign-in-button';
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center border-b-2 border-zinc-800 py-4 px-24">
-      <div className="flex items-center gap-20">
+    <header className="flex justify-between items-center border-b-2 border-zinc-800 py-4 px-24">
+      <nav className="flex items-center gap-20">
         <h1 className="text-2xl font-semibold">reading.news</h1>
 
         <div className="flex gap-8">
           <Link href="/">
-            <span className="text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 ">
+            <span className="text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 transition">
               Home
             </span>
           </Link>
           <Link href="/posts">
-            <span className="text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 ">
+            <span className="text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 transition">
               Posts
             </span>
           </Link>
         </div>
-      </div>
+      </nav>
 
-      <Link
-        href="/api/auth/signin"
-        className="flex items-center gap-4 bg-zinc-800 px-4 py-2 rounded-full"
-      >
-        <Github />
-        <span className="text-sm">Sing in with Github</span>
-      </Link>
-    </div>
+      <SignInButton />
+    </header>
   );
 }
