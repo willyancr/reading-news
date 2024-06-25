d18d03d06c50d51e93cc55334fc0c52d2df15cca
Willyan Costa <willyancr@gmail.com>
2024-06-25 14:12:17 -0300

    Update layout styles for reading news section

--- src/app/(reading-news)/layout.tsx
+++ src/app/(reading-news)/layout.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="min-h-screen w-full">
+    <section className="mx-auto min-h-screen w-full max-w-[1600px]">
       <Header />
       {children}
     </section>
