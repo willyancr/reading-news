ffa93c87633c319f103f192e78ffa0a9e42ec60c
Willyan Costa <willyancr@gmail.com>
2024-06-24 21:50:58 -0300

    Add Subscription Button and Improve Home Page Layout
    The commit adds a new `SubscribeButton` component and improves the layout of the home page. The main changes are:
    
    1. Imported the `SubscribeButton` component and used it in the home page.
    2. Refactored the HTML structure, dividing the content into two sections (text and image) for better organization.
    3. Applied a `max-w-[460px]` class to the image to ensure it doesn't exceed a specific width.
    4. Removed the unnecessary `group-hover:scale-105 transition-transform duration-150 ease-in` classes from the image, as they were not being used.
    
    These changes enhance the overall appearance and functionality of the home page, providing a better user experience.

--- src/app/(reading-news)/(home)/page.tsx
+++ src/app/(reading-news)/(home)/page.tsx
@@ -1,9 +1,10 @@
+import SubscribeButton from '@/components/subscribe-button';
 import Image from 'next/image';
 
 export default function HomePage() {
   return (
-    <div className="flex px-24 pt-32">
-      <div className="flex flex-col gap-6">
+    <main className="flex px-24 pt-32">
+      <section className="flex flex-col gap-6">
         <p>🖐️ Hey, welcome</p>
         <h1 className="text-5xl font-semibold">
           News about the <span className="text-blue-300">React</span> ecosystem
@@ -12,20 +13,18 @@ export default function HomePage() {
           <p>Get acess to all the publications</p>
           <p className="text-blue-300">for $9.90 month</p>
         </div>
-        <button className="bg-yellow-500 text-black w-48 font-bold py-2 rounded-full">
-          Subscribe now
-        </button>
-      </div>
-      <div>
+        <SubscribeButton />
+      </section>
+      <section>
         <Image
           src="/animation-home.png"
-          className="group-hover:scale-105 transition-transform duration-150 ease-in"
+          className="max-w-[460px]"
           width={860}
           height={860}
           quality={100}
           alt=""
         />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
