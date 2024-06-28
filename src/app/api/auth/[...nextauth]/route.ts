472e8ee512271acc3091d809478ffa940eb9679a
Willyan Costa <willyancr@gmail.com>
2024-06-27 22:12:45 -0300

    Add FaunaDB integration for user sign-in callback
    Integrate FaunaDB to handle user sign-in callback in NextAuth, checking
    and creating user records based on email.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import GitHubProvider from 'next-auth/providers/github';
 import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
+import { fauna } from '@/app/services/fauna';
 
 const handler = NextAuth({
   providers: [
@@ -13,6 +15,36 @@ const handler = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email as string),
+                ),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as string),
+              ),
+            ),
+          ),
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
