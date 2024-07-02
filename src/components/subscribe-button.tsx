d206c3336782ba6b9b6c4e02f1954082755518c4
Willyan Costa <willyancr@gmail.com>
2024-07-01 23:25:35 -0300

    update subscribe-button.tsx

--- src/components/subscribe-button.tsx
+++ src/components/subscribe-button.tsx
@@ -1,10 +1,25 @@
+'use client';
+
+import { signIn, useSession } from 'next-auth/react';
+
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  }
   return (
-    <button className="bg-yellow-500 text-black w-48 font-bold py-2 rounded-full hover:brightness-75 transition-all">
+    <button
+      onClick={handleSubscribe}
+      className="bg-yellow-500 text-black w-48 font-bold py-2 rounded-full hover:brightness-75 transition-all"
+    >
       Subscribe now
     </button>
   );
