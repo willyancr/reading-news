822631905793fd1fb88546aa22f1b3e9a188de17
Willyan Costa <willyancr@gmail.com>
2024-07-03 17:28:37 -0300

    update layout.tsx and layout.tsx

--- src/app/(reading-news)/layout.tsx
+++ src/app/(reading-news)/layout.tsx
@@ -2,6 +2,7 @@
 
 import Header from '@/components/header';
 import { SessionProvider } from 'next-auth/react';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
 const poppins = Poppins({
   weight: '400',
   subsets: ['latin'],
