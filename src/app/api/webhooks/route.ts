a2ddec84d58e2e1aadd56f0ef9b75c9277ff556c
Willyan Costa <willyancr@gmail.com>
2024-07-08 22:51:05 -0300

    Add saveSubscription function to handle checkout session completed webhook event
    - Import saveSubscription function to handle saving subscription data
      when checkout session completed webhook event is received.
    - Update POST function to call saveSubscription function for checkout
      session completed event type.

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { stripe } from '@/app/services/stripe';
 import { Readable } from 'stream';
 import Stripe from 'stripe';
+import { saveSubscription } from '../_lib/manageSubscription';
 
 const endpointSecret = process.env.STRIPE_WEBHOOKS_SECRET as string;
 
@@ -29,10 +30,10 @@ export async function POST(req: NextRequest) {
     const secret = req.headers.get('stripe-signature') as string;
 
     let event: Stripe.Event;
+
     try {
       event = stripe.webhooks.constructEvent(buf, secret, endpointSecret);
     } catch (err: any) {
-      console.error(`⚠️  Webhook signature verification failed.`, err.message);
       return NextResponse.json(
         { error: `Webhook Error: ${err.message}` },
         { status: 400 },
@@ -42,13 +43,24 @@ export async function POST(req: NextRequest) {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('Evento recebido', event);
+      switch (type) {
+        case 'checkout.session.completed':
+          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+          await saveSubscription(
+            checkoutSession.subscription?.toString() || '',
+            checkoutSession.customer?.toString() || '',
+          );
+
+          break;
+        default:
+          console.log(`🤷‍♀️ Unhandled event type: ${type}`);
+          break;
+      }
     }
 
     return NextResponse.json({ received: true });
-    
   } catch (err) {
-    console.error(err);
     return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: 500 },
