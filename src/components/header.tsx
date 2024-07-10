d1b15270b4d5d4ee018b385fdd9211e88c97a96b
Willyan Costa <willyancr@gmail.com>
2024-07-10 15:32:52 -0300

    update: Refactor header component styles and usePathname hook
    Extract header component styles into variables and utilize usePathname hook
    for active link highlighting. Improve code readability and maintainability.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link';
 import SignInButton from './sign-in-button';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const cssHeader =
+    'text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 transition';
+  const cssHeaderActive = 'text-zinc-50 border-b-2 border-yellow-500 pb-6';
+
   return (
     <header className="flex justify-between items-center border-b-2 border-zinc-800 py-4 px-24">
       <nav className="flex items-center gap-20">
@@ -9,12 +16,14 @@ export default function Header() {
 
         <div className="flex gap-8">
           <Link href="/">
-            <span className="text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 transition">
+            <span className={pathname === '/' ? cssHeaderActive : cssHeader}>
               Home
             </span>
           </Link>
           <Link href="/posts">
-            <span className="text-zinc-400 hover:text-zinc-50 hover:border-b-2 hover:border-yellow-500 hover:pb-6 transition">
+            <span
+              className={pathname === '/posts' ? cssHeaderActive : cssHeader}
+            >
               Posts
             </span>
           </Link>
